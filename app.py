89ce382ca2ed7a0fcfef5a188914eea45155a75c
rafatansari12 <rafatansari12feb1999@gmail.com>
2020-01-31 22:49:29 +0530

    Added functionalities like delete and update using sqlachemy db

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Quick note : 4 slashes--> Absolute path , 3 slashes--> Relative path
+db = SQLAlchemy(app)
+SQLALCHEMY_TRACK_MODIFICATIONS = False    # added to avoid a warning in terminald.
 
-@app.route('/')
+class Todo(db.Model):
+ id = db.Column(db.Integer,primary_key=True)
+ content = db.Column(db.String(200),nullable=False)
+ date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+ def __repr__(self):
+  return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
 def index():
- return render_template('index.html')
+  if request.method=='POST':
+    task_content = request.form['content']
+    new_task = Todo(content=task_content)
+    try:
+      db.session.add(new_task)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There is an error while adding task'
+  else:
+    tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+  task_to_delete = Todo.query.get_or_404(id)
+  
+  try:
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+  except:
+    return 'There was a problem while deleting that task'
 
- if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/update/<int:id>', methods=['POST','GET'])
+def update(id): 
+  task_to_update = Todo.query.get_or_404(id)
+  if request.method =='POST':
+    task_to_update.content = request.form['content']
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There is an error while updating your task'
+  else:
+    #tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('update.html', task_to_update=task_to_update)
+
+if __name__ == '__main__':
+  app.debug=True
